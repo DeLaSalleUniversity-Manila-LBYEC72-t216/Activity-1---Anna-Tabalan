5dfd2da4882f05babc03e1b8730d47e0b5e33259
anna-tabalan <anna.tabalan@gmail.com>
2016-05-26 16:37:28 +0800

    Create Activity 1.c

---
+++ Activity 1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf ("\n\n\n\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+	printf("\t\t  Anna Rovia V. Tabalan\n");
+	printf("\t\t\t BS ECE\n");
+	printf("\t\t\t11417315\n");
+	printf ("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n\n\n\n\n");
+}
